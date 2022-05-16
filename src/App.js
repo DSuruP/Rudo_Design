428c8b4a5cf729ed2173cdb841b335bb2141ff5c
Survesh Pandit <84669149+DSuruP@users.noreply.github.com>
2022-05-16 19:42:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Header } from './Components/Header';
+import { Middle } from './Components/Middle';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Middle />
+    </>
+  );
+}
+
+export default App;
